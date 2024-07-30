20f9c149e8a9a80848702ac1fa5a250adc9f7753
chen <205630746@qq.com>
2024-07-30 17:32:38 +0800

    标记

--- src/views/common/map/index.vue
+++ src/views/common/map/index.vue
@@ -10,14 +10,19 @@ import {getPosition} from "@/utils/index"
 const containerRef = ref(null)
 
 let map:any = null;
+let marker:any = null
  
 onMounted(() => {
      let longitude:number, latitude:number
+    //  获取定位
     getPosition().then((res:any)=>{
         console.log(res.longitude);
          longitude = res.longitude
          latitude = res.latitude
     })
+    window._AMapSecurityConfig = {
+        securityJsCode :'e481caa600cf3b3d9b5442c9577b5d09'
+    }
   AMapLoader.load({
     key: "568cd9b29b0aca844b8934ea56ba9cc9", // 申请好的Web端开发者Key，首次调用 load 时必填
     version: "2.0", // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
@@ -31,6 +36,13 @@ onMounted(() => {
         // center:[longitude, latitude], // 初始化地图中心点位置
         mapStyle: "amap://styles/normal",//设置地图的显示样式可选样式有normal、grey、whitesmoke、dark、light、graffiti，切换字符串最后单词即可
       });
+    //   标记
+      marker = new AMap.Marker({
+          icon: "//a.amap.com/jsapi_demos/static/demo-center/icons/poi-marker-default.png",
+          position: [longitude, latitude],
+        //   offset: new AMap.Pixel(20, -30)
+        });
+        marker.setMap(map)
     })
     .catch((e) => {
       console.log(e);
